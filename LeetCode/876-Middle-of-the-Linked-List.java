c18fa13348121aac0ce088dab91590dfb99f1d8e
Mayuri Sampathgiri <80785055+mayurisampathgiri@users.noreply.github.com>
2022-12-27 17:09:28 -0500

    Create 876-Middle-of-the-Linked-List.java

---
+++ LeetCode/876-Middle-of-the-Linked-List.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int len = 0;
+        while(head!=null) {
+            len = len+1;
+            head = head.next;
+        }
+        head = temp;
+        int fin = len/2;
+        while(fin > 0) {
+            head = head.next;
+            fin--;
+        }
+        return head;
+    }
+}
+
+//TC : 
